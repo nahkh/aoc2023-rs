08c07cd23878bb4e5f941f3a4097c16046eba383
Juuso Valli <juuso.valli@gmail.com>
2023-12-02 06:59:31 +0100

    Day 2 - part 1

--- src/day02.rs
+++ src/day02.rs
@@ -1,9 +1,78 @@
 use crate::input_files::get_current_day;
 use crate::input_files::read_content;
 use std::path::Path;
+use regex::Regex;
+
+#[derive(Debug, PartialEq)]
+struct Observation {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Observation {
+    fn parse(content: &str) -> Observation {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+        for part in content.trim().split(',') {
+            let re = Regex::new(r"\s*(\d+) (\w+)").unwrap();
+            let captures = re.captures(part).unwrap();
+            let number = captures.get(1).unwrap().as_str().parse::<u32>().ok().unwrap();
+            let color = captures.get(2).unwrap().as_str();
+            match color {
+                "red" => red = number,
+                "green" => green = number,
+                "blue" => blue = number,
+                _ => panic!("Unparseable color {} in {}", color, part)
+            }
+        }
+        Observation {red, green, blue}
+    }
+
+    fn is_possible(&self, actual_red: u32, actual_green: u32, actual_blue: u32) -> bool {
+        actual_red >= self.red && actual_green >= self.green && actual_blue >= self.blue
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    observations: Vec<Observation>
+}
+
+impl Game {
+    fn parse(content: &str) -> Game {
+        let mut parts = content.trim().split(':');
+        let id = parts.next().unwrap()[5..].parse::<u32>().ok().unwrap();
+        let mut observations = Vec::new();
+        for observation_part in parts.next().unwrap().split(';') {
+            observations.push(Observation::parse(observation_part));
+        }
+
+        Game {id, observations}
+    }
+
+    fn is_possible(&self, actual_red: u32, actual_green: u32, actual_blue: u32) -> bool {
+        for observation in self.observations.iter() {
+            if !observation.is_possible(actual_red, actual_green, actual_blue) {
+                return false;
+            }
+        }
+
+        true
+    }
+}
 
 fn part1(content: &str) {
-    println!("Part 1 not implemented");
+    let mut total = 0;
+    for line in content.lines() {
+        let game = Game::parse(line);
+        if game.is_possible(12, 13, 14) {
+            total += game.id;
+        }
+    }
+    println!("Part 1: The sum of ids of possible games is {}", total);    
 }
 
 fn part2(content: &str) {
