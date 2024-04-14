8abb70278ea186b76987a42fa56722de3191d927
Juuso Valli <juuso.valli@gmail.com>
2024-04-14 20:53:09 +0200

    Day 6

--- src/day06.rs
+++ src/day06.rs
@@ -2,17 +2,95 @@ use crate::input_files::get_current_day;
 use crate::input_files::read_content;
 use std::path::Path;
 
-fn part1(_content: &str) {
-    println!("Part 1 not implemented");
+
+fn distance_traveled(duration: u64, charge_time: u64) -> u64 {
+    if charge_time == 0 {
+        return 0;
+    }
+    if charge_time >= duration {
+        return 0;
+    }
+    
+    charge_time * (duration - charge_time)
 }
 
-fn part2(_content: &str) {
-    println!("Part 2 not implemented");
+#[derive(Debug, PartialEq)]
+struct Race {
+    duration: u64,
+    record: u64,
+}
+
+impl Race {
+    fn new(duration: u64, record: u64) -> Race {
+        Race {
+            duration,
+            record,
+        }
+    }
+
+    fn possible_winning_strategies(&self) -> u64 {
+        let mut output = 0;
+
+        for i in 1..self.duration {
+            if distance_traveled(self.duration, i) > self.record {
+                output += 1;
+            }
+        }
+
+
+        output
+    }
+}
+
+fn parse_input(content: &str) -> Vec<Race> {
+    let mut races = Vec::new();
+    let lines = content.lines().collect::<Vec<&str>>(); 
+    let times = lines.get(0).expect("There should be at least one line in the input").split_whitespace().collect::<Vec<&str>>();
+    let distances = lines.get(1).expect("There should be at least two lines in the input").split_whitespace().collect::<Vec<&str>>();
+    for i in 1..times.len() {
+        races.push(Race::new(times.get(i).unwrap().parse::<u64>().ok().unwrap(), distances.get(i).unwrap().parse::<u64>().ok().unwrap()));
+    }
+
+    races
+}
+
+fn number_of_digits(value: u64) -> u64 {
+    value.to_string().len().try_into().unwrap()
+}
+
+fn concat_digits(a: u64, b: u64) -> u64 {
+    a * (10^number_of_digits(a)) + b
+}
+
+
+fn part1(content: &str) {
+   let mut output = 1;
+   for race in parse_input(content) {
+     output *= race.possible_winning_strategies();
+   }
+
+   println!("Part 1: {} possible solutions", output);
+}
+
+fn part2(content: &str) {
+   let mut time = 0;
+   let mut record = 0;
+   for race in parse_input(content) {
+     time = concat_digits(time, race.duration);
+     record = concat_digits(record, race.record);
+   }
+
+   let combined_race = Race::new(48938595, 296192812361391);
+   println!("{:?}", combined_race);
+
+   let output = combined_race.possible_winning_strategies();
+
+   println!("Part 2: {} possible solutions", output);
 }
 
 pub fn execute() {
     let current_day = get_current_day(file!());
-    let input_file = format!("data\\day{:02}.txt", current_day);
+    let input_file = format!("data{}day{:02}.txt", std::path::MAIN_SEPARATOR_STR, current_day);
     if !Path::new(&input_file).exists() {
         println!("No input file for day {}", current_day);
         return;
--- src/input_files.rs
+++ src/input_files.rs
@@ -5,7 +5,7 @@ pub fn read_content(filename: &str) -> String {
 }
 
 pub fn get_current_day(code_file: &str) -> i32 {
-    let re = Regex::new(r"src\\day(\d\d).rs").unwrap();
+    let re = Regex::new(format!(r"src{}day(\d\d).rs", std::path::MAIN_SEPARATOR_STR).as_str()).unwrap();
     let captures = re.captures(code_file).unwrap();
 
     captures
